d205c3516913d77671d192b17279725dad4d6126
bruce-69 <eduardcrow555@gmail.com>
2022-03-26 09:28:41 -0300

    progeto3

---
+++ progeto3.js
@@ -0,0 +1,51 @@
+
+var prompt = require('prompt-sync')();
+console . log ('apresentacao                            ')
+
+
+
+// barbar gordao filha do comisario de pulicia jim gordao . recem aceita da univercidade de gotham 
+// seu obijetivo é chegar en casa viva  , a sua rotina é traalhoa 8 horas na bliblioteca , 8 da noite faculdade 
+
+// personagens batman alfred jim gordom dick graysom luke fox demian wayne
+
+var saude = 10 
+var bateria_celular = 100
+var dinhero = 230
+var bat_facas  = 4
+var casaço = 10
+//coreçao de  testo 
+
+
+
+
+function rotina1 ( )  {
+    var manha = prompt('cidade noava vida , o despertador toca vc acorda  ')
+    var cafe = prompt ('para o cafe vc tem duas opições a saldavel é chata ou a rapida é godtosa ')
+    var ir_tabalhar = console.log  ('sao 8 da mannha vc tem q ir ao trabalho ')
+    var bliblioteca = prompt ('ao chegar a bliblioteca oq vc faz primeiro , cafe ou organisa os arquivos ')
+
+ 
+                            
+
+
+}
+
+
+
+
+function rotina2 ( ) {
+var sai_tranpo = prompt('sao 8 meia da noite ')
+var faculdade = prompt('hoje é dia de prova vc se preparol mas  esa cansada  ')
+
+}
+//confrito da historia q define se vc vai ver o batman ou nao 
+
+
+
+function rotina ( ) {
+    var sai_tranpo = prompt('vc passa por um beco escuro ')
+    var sai_tranpo = prompt('vc ouve um grito  ')
+}
+
+//final da historia ou vai para o hospital e ve o batman ou vai pra casa sam e salva 
