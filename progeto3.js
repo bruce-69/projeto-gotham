00f8eb489791cdb90a3166271465e9d4fedbf767
bruce-69 <eduardcrow555@gmail.com>
2022-03-27 18:53:22 -0300

    rascunho da histria esqueleto do codigo

--- progeto3.js
+++ progeto3.js
@@ -1,6 +1,6 @@
 
 var prompt = require('prompt-sync')();
-console . log ('apresentacao                            ')
+console . log ('apresentacao    esse jogo se passa em gotham    e a personagem pricipal é barbara gordom a batgirl \n uma das pesornagem do universo batman \n o seu obigetivo é chegar em casa viva  ' )
 
 
 
@@ -12,7 +12,7 @@ console . log ('apresentacao                            ')
 var saude = 10 
 var bateria_celular = 100
 var dinhero = 230
-var bat_facas  = 4
+var armas   = 4
 var casaço = 10
 //coreçao de  testo 
 
@@ -20,23 +20,66 @@ var casaço = 10
 
 
 function rotina1 ( )  {
-    var manha = prompt('cidade noava vida , o despertador toca vc acorda  ')
-    var cafe = prompt ('para o cafe vc tem duas opições a saldavel é chata ou a rapida é godtosa ')
-    var ir_tabalhar = console.log  ('sao 8 da mannha vc tem q ir ao trabalho ')
-    var bliblioteca = prompt ('ao chegar a bliblioteca oq vc faz primeiro , cafe ou organisa os arquivos ')
+      
 
- 
-                            
 
+    var manha = prompt(' 7  horas o despertador toca vc acorda1  ? ou dormi mas um pouco 2')
+    var cafe = prompt ('para o cafe vc tem  cafe e toradas1 ou baicom com ovos2  ')
+    var ir_tabalhar = prompt ('sao 8 da mannha vc tem q ir ao trabalho  1 vc  pode ir de moto ou  2 vc pode pegar carona com oseu pai ')
+
+
+    var bliblioteca = prompt ('vc chega apo trabalho as 9 e 14 oxq vc faz o cafe1 ou organisa os arquivos2 ')
+    console.log (' o tenpo passa de vagar as pessoas entao e sain , vc comversa com alguns amigos ')
+
+
+ if (manha == 1) {
+  console.log (' ao acordar vc ve seu pai preparando um Café Irlandês meio apresado ')
+
+
+
+ }else if (manha ==  2 )  { 
+var atrso = prompt('se passa uma hora vc corda com as ligacoes do trabalho 1oq vc faz corre para o trabalho sem comer , 2 pega um pao  e corre para a sua moto ')
+
+
+ }if ( ir_tabalhar == 1    ){   
+console.log( ' vc se atrasa um pouco mas chega a blioteca ')
+
+
+
+ }if (ir_tabalhar == 2       ) { 
+console.log (' quando vc fala com o gordom ele recebe uma ligaccao urgemte e sai sem te responder  , ')
+console.log (' vc chega a sua moto uma r3 roza , durante a viagen a sua moto trava o pedal de canbio vc consegue evitar um asidenti depis de meia horas é moitas ligacoes do trabalho')
+console.log('vc reslove ligar para o dick  ele chega é em 5 minutos resolve o problema , vc chega ao trabalho atrasada   45  minutos ')
+
+
+
+
+
+ }
+
+
+  
+                
 
 }
 
+  
+                  
 
 
 
 function rotina2 ( ) {
-var sai_tranpo = prompt('sao 8 meia da noite ')
-var faculdade = prompt('hoje é dia de prova vc se preparol mas  esa cansada  ')
+var sai_tranpo = console.log(' agora sao 9 horas , 1 vc tem alguns docmentos novos para organisar , alguns livros  novos para catalogar ç   ')
+var faculdade = console.log(' depdo de  algum tenpo vc temina as suas atividades    oq vc faz agora 1pega um cafe  , ou 2 adianta a materia da facudade ')
+var dick = prompt('')
+if ( dick  ==1   ){
+console.log('vc rebe uma mensagen 1 vc olha 2 nao ')
+
+}else if ( dick == 2 ){
+casaço - 1 
+console.log(' durante os estudos ')
+}
+
 
 }
 //confrito da historia q define se vc vai ver o batman ou nao 
@@ -48,4 +91,6 @@ function rotina ( ) {
     var sai_tranpo = prompt('vc ouve um grito  ')
 }
 
-//final da historia ou vai para o hospital e ve o batman ou vai pra casa sam e salva 
+
+
+//final da historia ou vai para o hospital e ve o batman ou vai pra casa sam e salva
