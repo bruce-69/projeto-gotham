e79da038dd57eb14510dd714e3a859e1d812d68f
bruce-69 <eduardcrow555@gmail.com>
2022-03-29 17:45:30 -0300

    mudanças no progeto

--- progeto3.js
+++ progeto3.js
@@ -1,4 +1,4 @@
-
+    
 var prompt = require('prompt-sync')();
 console . log ('apresentacao    esse jogo se passa em gotham    e a personagem pricipal é barbara gordom a batgirl \n uma das pesornagem do universo batman \n o seu obigetivo é chegar em casa viva  ' )
 
@@ -15,52 +15,90 @@ var casaço = 10
 //coreçao de  testo 
 
 function rotina1 ( )  {
-      
+      //mais opisoes deixar mais claro ir para a faculdade 
     var manha = prompt(' 7  horas o despertador toca vc acorda1  ? ou dormi mas um pouco 2')
-    var cafe = prompt ('para o cafe vc tem  cafe e toradas1 ou baicom com ovos2  ')
-    var ir_tabalhar = prompt ('sao 8 da mannha vc tem q ir ao trabalho  1 vc  pode ir de moto ou  2 vc pode pegar carona com oseu pai ')
+if (manha == 1 ){
+console.log(' ao acordar vc ve sua casa bagunsado e seu pai indo para o trabalho  , oq vc faz 1 pergunta aonde ele vai 2 , espera ele sair ele liga para seu namorado  , 3 se aruma para o trabalho ')
+ var acordar = prompt()
+}else if ( acordar == 1 ){
+console.log(' seu pai diz vou ao trabalho querida , parece q ouve um fugitivo em  arkham ')
 
 
-    var bliblioteca = prompt ('vc chega apo trabalho as 9 e 14 oxq vc faz o cafe1 ou organisa os arquivos2 ')
-    console.log (' o tenpo passa de vagar as pessoas entao e sain , vc comversa com alguns amigos ')
+}if ( acordar == 2 ){
+console.log('ele atende mas diz q esta ocupado talvez eu te encontre na faculdade ')
+
+}if (acordar == 3 ){
+console.log(' vc se arruma toma cafe   \n coomo vc quer ir para o trbalho  1 ape 2 de moto  ou 3 liga para o demian e pede carona ')
+var deimian = pprompt ( ' ')
 
 
- if (manha == 1) {
-  console.log (' ao acordar vc ve seu pai preparando um Café Irlandês meio apresado ')
-
-
-
- }else if (manha ==  2 )  { 
-var atrso = prompt('se passa uma hora vc corda com as ligacoes do trabalho 1oq vc faz corre para o trabalho sem comer , 2 pega um pao  e corre para a sua moto ')
-
-
- }if ( ir_tabalhar == 1    ){   
-console.log( ' vc se atrasa um pouco mas chega a blioteca ')
+}if ( deimian == 1 ){
+console. log ('  depois de uma caminhada de  20 minutos dick passa por vc de carro é overece carona   ')// fazer algo mais 
 
 
 
- }if (ir_tabalhar == 2       ) { 
-console.log (' quando vc fala com o gordom ele recebe uma ligaccao urgemte e sai sem te responder  , ')
-console.log (' vc chega a sua moto uma  durante a viagen a sua moto trava o pedal de canbio vc consegue evitar um asidenti q vc faz 1 tenta conccertar ou 2 liga para o dick ')
-var moto_quebada = prompt('')
+}if ( deimian == 2  ){
+console. log ('  vc chega a faculdade  depois de alguns minutos   ')
 
-console.log('vc reslove ligar para o dick  ele chega é em 5 minutos resolve o problema , vc chega ao trabalho atrasada   45  minutos ')
 
- }
-                
+
+
+
+}if ( deimian == 3   ){
+    console. log ('  ele atende  e pergunta oq vc quer , vc responde  quer carona para a faculdade   \n  ele diz nao oq vc responde 1 tchau ou 2 manda ele se fuder ')
+    var conversa = prompt('')
+    
+    
+
+
+}if ( conversa == 1    ){
+    console. log ('  ele disliga')
+    
+    
+    
+} if (conversa == 2 ){
+     console.log(' vai vc tu tu tu ')
 
 }
 
-function rotina2 ( ) {
-var sai_tranpo = console.log(' agora sao 9 horas , 1 vc tem alguns docmentos novos para organisar , alguns livros  novos para catalogar ç   ')
-var faculdade = console.log(' depdo de  algum tenpo vc temina as suas atividades    oq vc faz agora 1pega um cafe  , ou 2 adianta a materia da facudade ')
-var dick = prompt('')
-if ( dick  ==1   ){
-console.log('vc rebe uma mensagen 1 vc olha 2 nao ')
 
-}else if ( dick == 2 ){
-casaço - 1 
-console.log(' durante os estudos vc coxilou , vc acorda com o demina ti encarando , te da um sanduiche , \ne diz  quer tem motrar uma coisa ,  vc sabe q provavelmente é mintira . 1 vc entra no jogo dele 2 vc espulsa ele como senpre ')
+
+}
+
+
+
+
+
+
+
+ 
+                
+
+
+// faculdade dimuiir estatos usar hora 
+function rotinafacu ( ) {
+var faculdade = console.log('  ja na faculdade o vc vai 1 direto parra as aulas  2 toma um cafe 3 vai para a biblioteca estudar  ')
+
+var  facu = prompt('')
+if ( facu   ==1   ){
+console.log( ` hoge vc tem essas aulas sao Algoritmos e Programação, 
+ Análise e Projeto de Algoritmos,
+ Análise e Projeto de Software,
+ Arquitetura e Organização de Computadores I,`)
+
+
+
+
+
+
+
+}else if ( facu  == 2 ){
+
+console.log('durante a primeira a primeira aula o professor faz uma pergunta oq é um algoritimo , oq vc responde  ')
+
+
+
+
 }
 console.log(' vc tem q estar na faculdade as 5 \n quando vc acabade comer algumas pessoas chegam vc vai falar com eles , e ti dao uma lista de livros 1984, de George Orwell\nO Lobo da Estepe \n Dom Casmurro\n meu desfruto \n cem chances')
 console.log('algus vao enbora ficam apenas 2  , 1 vc se senta com eles ou 2 volta ao trabalho    ')
@@ -71,23 +109,23 @@ console.log(' vc se destrai com a cconversa e esquece da facudade as 5 horas vc 
 //confrito da historia q define se vc vai ver o batman ou nao 
 
 
-function facudade ( ) {  
-console.log(` hoje vc essas aulas Algoritmos e Programação, 
-Análise e Projeto de Algoritmos,
-Análise e Projeto de Software,
-Arquitetura e Organização de Computadores I,
-Arquitetura e Organização de Computadores II,
-Banco de Dados I,
-Banco de Dados II, 
-Circuitos Digitais`)
-     console.log('vc sai da faculdade cansanda sao 11 e meai da noite  , muita cansanda vc qur sair da faculdade mas um grupo te pedi ajuda para estudar ' )
-  console.log(' muito cansada depois das aulas oq vc faz 1 vai sosinah ou 2 pede carona 2 ou 3 oferece cara para alguem ')   
-var voltarcasa1 = prompt('')
+// function facudade ( ) {  
+// console.log(` hoje vc essas aulas Algoritmos e Programação, 
+// Análise e Projeto de Algoritmos,
+// Análise e Projeto de Software,
+// Arquitetura e Organização de Computadores I,
+// Arquitetura e Organização de Computadores II,
+// Banco de Dados I,
+// Banco de Dados II, 
+// Circuitos Digitais`)
+//      console.log('vc sai da faculdade cansanda sao 11 e meai da noite  , muita cansanda vc qur sair da faculdade mas um grupo te pedi ajuda para estudar ' )
+//   console.log(' muito cansada depois das aulas oq vc faz 1 vai sosinah ou 2 pede carona 2 ou 3 oferece cara para alguem ')   
+// var voltarcasa1 = prompt('')
 
 // diminuir istatos 
 
 
-}
+
 
 
 function rotina3( ) {
@@ -116,3 +154,4 @@ console.log (' durante a briga vc ouve um barulho das sonbras sugem o batman ele
 
 
 //final da historia ou vai para o hospital e ve o batman ou vai pra casa sam e salva
+    rotina1()
