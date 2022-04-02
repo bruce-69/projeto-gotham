53952bf5fac7e5a83266b0f912eebdf3cd1c39c6
bruce-69 <eduardcrow555@gmail.com>
2022-04-02 20:24:08 -0300

    acabando

--- projeto3.js
+++ projeto3.js
@@ -7,7 +7,6 @@ var prompt = require('prompt-sync')()
  }
 let batgirl={    
 saude:10,
-armas:4,
 casaço:10,                                   
 forca:9,
 golpe:4,
@@ -16,17 +15,25 @@ let bandido={
 saude:5,
 forca:4,
 golpe2:1,
+facada:2,
+tiro:4,
 };  
+function bandido_aleatorio(){
+let lista = [bandido.facada,bandido.tiro,bandido.golpe2];
+ let dano = lista = [Math.floor(Math.random()* 3)];
+batgirl.saude-=dano
+}
+function bandido_estatus(batgirlgolpe , bandidosaude){
+ console.log(bandido.saude-batgirl, )
+}
 function batalha(saude , golpe ){
-console.log('vida inimigo - ')   
-console.log(  saude  = saude - golpe)
+console.log(  saude = saude- golpe)
 }
 function estatos (saude , casaço ){
  console.log('estatos personagen ')   
-console.log( saude )
-console.log(casaço)
+console.log( saude ),console.log('vida')
+console.log(casaço ),console.log('energia')
 }
-//primeira batalha
 for (let primeira_missao = 0; primeira_missao < 2; primeira_missao++) {
 console.log('derepente no radio da pulicia vc escuta atençao central temos um 10 14 na rua  brownsville numero 10  \n vc vai ate la 1 sim , 0 nao ')
 primeira_missao = prompt()
@@ -36,13 +43,28 @@ if( primeira_missao = 2 )
 console.log(' quando vc chega ao local vc ve um casal descutindo ele parece nervoso esta com uma faca na mao ela esta com\n  um olho roxo , o suspeito ja tem pasagem pela pulicia por feminicídio')
  console.log(' 1 chega com calma tentando alcamalos ou 0 ataca sem do nen piedade ')
  let casal = prompt('')
-    if ( casal <= 0 )
- batalha(bandido.saude , batgirl.golpe ) , console.log(' vc acerta o primeiro soco ele ainda esta meio sonzo qual golpe  vc quer usar 1 ou 2 ')
-let casalluta=prompt('')
-if( casalluta= 2  || 1)
-batalha(bandido.saude , batgirl.golpe )
-batalha(bandido.saude , batgirl.golpe ),console.log('vc  acerta mais dois socos ele cai e vc ajuda a garota a pulicia chega é vc vai enbora ')
-if(casal = 1 )
-console.log('vc conten a situacao ate pucia chegar ')
-//segunda parte batalhas genericas diminuir estatus
-estatos(saude , casaço)
+    if ( casal == 0 )
+ console.log(' vc acerta o primeiro soco ele ainda esta meio sonzo qual golpe  vc quer usar 1 ou 2 ')
+let briga = prompt()
+ if(casal == 1 )
+ console.log('vc conten a situacao ate a pulicia chegar ')
+ if( briga == 2 ||1  ){
+  batalha(bandido.saude , batgirl.golpe ),console.log('vc  acerta mais dois socos ele cai e vc ajuda a garota a pulicia chega é vc vai enbora ')
+ estatos(batgirl.saude ,batgirl.casaço-1)}
+ function proximo1 ( saude , golpe , casaço, golpe2 ){
+console.log('mais uma hora se passa  agora sao 00 da noite é o radio taca  mais uma vez , vc vai te o local \n  1 sim 2 nao ')
+let chamado_alternativo2 = prompt()
+if ( chamado_alternativo2 == 1 )
+console.log( ' vc chega ao local é encontra  3 inimigos ')
+ }
+function mais_uma_hora ( saude , golpe , casaço, golpe2 ,faca ,tiro ){
+console.log('mais uma hora se passa  agora sao 11 da noite é o radio taca , vc vai te o local \n  1 sim 2 nao ')
+let chamado2 = prompt()
+if ( chamado2 == 1 )
+console.log( ' vc chega ao local é encontra 5 inimigos vc enfrenta o primeiro ')
+if ( chamado2 == 2 )
+proximo1()
+}
+mais_uma_hora()
+
+//terminar 
